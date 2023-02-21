13fc1e70c558524119d0cdc88244fd5d1e190f25
Nonso024 <josephani772@gmail.com>
2023-02-21 01:40:20 -0800

    additional fixes

--- sort.h
+++ sort.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,19 +33,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *, size_t);
-void print_list(const listint_t *);
-void print_list_rev(const listint_t *);
-void bubble_sort(int *, size_t);
-void insertion_sort_list(listint_t **);
-void swap_nodes(listint_t **, listint_t *, listint_t *);
-void selection_sort(int *, size_t);
-void quick_sort(int *, size_t);
-void _qsort(int *, int, int, int);
-void shell_sort(int *, size_t);
-void cocktail_sort_list(listint_t **);
-void counting_sort(int *, size_t);
-void merge_sort(int *, size_t);
-void heap_sort(int *, size_t);
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-#endif
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
